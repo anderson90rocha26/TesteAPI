84c5d03a746b7e093198beba9ffa3f3227d71a2c
anderson90rocha26 <anderson90rocha26@gmail.com>
2024-05-17 15:51:18 -0300

    Incluir a class AppDbContext

--- TesteAPI/obj/Debug/net8.0/TesteAPI.AssemblyInfo.cs
+++ TesteAPI/obj/Debug/net8.0/TesteAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342873df031bb89cafa2f56f29e7947a51b8dabc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab126ad7787cc179088a2f4c2743a5e4e74a1e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
